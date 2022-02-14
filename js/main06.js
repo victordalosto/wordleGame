af236d75e12d326293d74f4bb18275e72ac55c8c
VictorDalosto <victordalosto@gmail.com>
2022-02-14 01:11:37 -0300

    version 0.6

--- js/main06.js
+++ js/main06.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     var currentTry = 0;
     var guessedWord = [];
 
-    const optionsColor = ["rgb(59, 153, 50)", "rgb(211, 178, 32)", "rgb(122, 122, 131)"] // correctBox, semiCorrectBox, wrongBox
+    const optionsColor = ["rgb(59, 153, 50)", "rgb(211, 178, 32)", "rgb(100, 100, 110)"] // correctBox, semiCorrectBox, wrongBox
 
-    const possibleLetters = ['q' ,'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'à', 'á', 'â', 'â', 'í', 'é', 'ê', 'ó', 'ô', 'õ', 'ú', 'ç', 'ñ'];
-    const dayWord = getNewWord();
+    const possibleLetters = ['q' ,'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'à', 'á', 'â', 'â', 'í', 'é', 'ê', 'ó', 'ô', 'õ', 'ú', 'ç', 'ñ'];
+    const dayWord = todaysWord();
     const numTrys = 6;
     createSquares(dayWord.length, numTrys);
     startAnimations();
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
         // RABETAO;
         // const dayWord = "MALANDRAMENTE";
         // TCHELEKA;
-        // TCHUTCHUKA;
-        const dayWord = "TCHUTCHUKA";
+        // TCHUTCHUCA;
+        const dayWord = "TCHUTCHUCA";
         return dayWord;
     }
     
@@ -110,41 +110,31 @@ document.addEventListener("DOMContentLoaded", () => {
             const value = lista[j];
             window.setTimeout(()=>{boxes[iterable].classList.add("animationFlip"); }, time);
             window.setTimeout(()=>{boxes[iterable].classList.add(value); }, time+300);
-            
+            time = time + 150;
             keyboard.forEach(botao => {
                 if (botao.textContent == word[j]) {
                     const currentColor = botao.style.backgroundColor;
                     if (lista[j] == "correctBox" && currentColor != "correctBox"){
                         botao.style.backgroundColor = optionsColor[0];
                         botao.style.color = "white";
-                        botao.classList.add("animationPop");
-                        window.setTimeout(()=>{botao.classList.remove("animationPop")}, 150);
+                        botao.classList.add("animationKBDPop");
+                        window.setTimeout(()=>{botao.classList.remove("animationKBDPop")}, 200);
                     } else 
                     if (lista[j] == "semiCorrectBox" && currentColor != "correctBox" && currentColor != "semiCorrectBox"){
                         botao.style.backgroundColor = optionsColor[1];
                         botao.style.color = "white";
-                        botao.classList.add("animationPop");
-                        window.setTimeout(()=>{botao.classList.remove("animationPop")}, 150);
+                        botao.classList.add("animationKBDPop");
+                        window.setTimeout(()=>{botao.classList.remove("animationKBDPop")}, 200);
                     } else {
                         botao.style.backgroundColor = optionsColor[2];
                         botao.style.color = "white";
-                        botao.classList.add("animationPop");
-                        window.setTimeout(()=>{botao.classList.remove("animationPop")}, 150);
-                    }
-                    }
-            })
-
-            time = time + 150;
+                        botao.classList.add("animationKBDPop");
+                        window.setTimeout(()=>{botao.classList.remove("animationKBDPop")}, 200);
+            }}})
         }
-        
-        if (fixWord(word) == fixWord(dayWord)){
-            credits();
-            return;
-        } 
-
-        if (CT + dayWord.length >= numTrys * dayWord.length){
-            credits();
-        } else {
+                if (fixWord(word) == fixWord(dayWord) || CT + dayWord.length >= numTrys * dayWord.length){
+            window.setTimeout(()=>{credits(time);}, time+1000);
+        }  else {
             currentTry += dayWord.length
             guessedWord = [];
         }
@@ -152,9 +142,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    function credits(){
+    function credits(time){
         gameEnded = true;
-        document.getElementById("game__keyboard").remove();
+        document.querySelectorAll("[data-key]").forEach(keey => {
+            {keey.classList.add("animationKeyBoardGone");}}); 
+        const t = document.querySelectorAll(".shareButton")
+        window.setTimeout(()=>{t[0].style.visibility = "visible";}, 500);
+        share(dayWord.length)   
     }
 
 
@@ -163,7 +157,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if (gameEnded == true) {
             return;
         }
-        
         if (fixWord(letter) == "enter"){
             if (guessedWord.length == dayWord.length){
                 var word = "";
@@ -172,6 +165,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
                 updateAfterEnter(fixWord(word), fixWord(dayWord));
             } else {
+                const nots = document.getElementById("game__notification");
+                if (!nots.classList.contains("animationNotification")){
+                    const possibleAlerts = ["CALMA PAPAI", "CALMA NA SENTADA", "CALMA NA SARRADA", "PALAVRA INCOMPLETA", "SEGURA AÍ", "CALMA"]
+                    nots.classList.add("animationNotification");
+                    nots.textContent = possibleAlerts[Math.floor(Math.random()*possibleAlerts.length)];
+                    window.setTimeout(()=>{nots.classList.remove("animationNotification")}, 2500)
+                }
                 var boxes = document.querySelectorAll(".square") 
                 for (let j=0; j< dayWord.length; j++) {
                     const iterable = j+currentTry;
@@ -205,9 +205,10 @@ function createSquares(num, numTrys) {
     const clientWidth = document.getElementById('game__board').clientWidth;
     
     const spaceX = clientWidth/(6*num+3);
-    const spaceY = Math.min(clientHeight/(6*numTrys+3), 1.5*spaceX);
-    const squareX = spaceX*5;
-    const squareY = Math.min(spaceY*5, squareX);
+    const spaceY = Math.min(clientHeight/(6*numTrys+9), 1.5*spaceX);
+    const squareX = Math.min(spaceY*5, spaceX*5);
+    const squareY = Math.min(spaceY*5, spaceX*5);
+
 
 
     gameBoard.style.gridTemplateColumns = "1fr ".repeat(num) // Set Number of elements in a rows
@@ -217,17 +218,86 @@ function createSquares(num, numTrys) {
         square.classList.add("square");
         square.setAttribute("id", index + 1);
         gameBoard.appendChild(square);
-        square.style.width = `${0.95*squareX}px`;
-        square.style.height = `${0.95*squareY}px`;
-        square.style.fontSize = `${0.70*squareX}px`;
+        square.style.width = `${0.90*squareX}px`;
+        square.style.height = `${0.90*squareY}px`;
+        square.style.fontSize = `${0.6*squareX}px`;
     }
     gameBoard.style.gap = `${spaceY}px ${spaceX}px`;
-    gameBoard.style.marginLeft = `${2*spaceX}px`;
-    gameBoard.style.marginRight = `${2*spaceX}px`;
+    gameBoard.style.marginLeft = `${5*spaceX}px`;
+    gameBoard.style.marginRight = `${5*spaceX}px`;
 }
 
 function startAnimations(){
     document.querySelectorAll(".square").forEach(box => {box.classList.add("animationBegin"); window.setTimeout(()=>{box.classList.remove("animationBegin")}, 400);}); // Play Fade in box Animation
     document.querySelectorAll("[data-key]").forEach(keey => {{keey.classList.add("animationKeyboardBegin"); window.setTimeout(()=>{keey.classList.remove("animationKeyboardBegin");}, 1500);}}); // Play Fade in keyboard Animation
 }
-  
+
+
+function todaysWord(){
+    const today = new Date();
+    var dateDay = today.getDate();
+    var dateMonth = today.getMonth()+1;
+    const dateYear = today.getFullYear();
+    var word = "TOP";
+    if (dateDay < 10){
+        dateDay = "0" + dateDay;
+    }
+    if (dateMonth < 10){
+        dateMonth = "0" + dateMonth;
+    }
+    const fullDate = dateDay+"/"+dateMonth+"/"+dateYear;
+    var request = new XMLHttpRequest();
+    request.open("GET", '../assets/data.json', false)
+    request.send(null);
+    var wordsList = JSON.parse(request.responseText);
+    wordsList = wordsList.FUNKWORDS;
+    wordsList.forEach((funkdodia)=>{
+        if (funkdodia.DATA == fullDate){
+            word = funkdodia.WORD;
+    }});
+    return word;
+}
+
+
+function share(num){
+    const boxCorrect = "🟩";
+    const boxSemiCorrect = "🟨";
+    const boxWrong = "⬛";
+    const possibleTitles = ["So baixaria no", "Joguei o", "Arrasei no", "Deitei no", "Sarrei no", "Rebolei no", "Martelei no"]
+    var title = possibleTitles[Math.floor(Math.random()*possibleTitles.length)];
+
+    const boxes = document.querySelectorAll(".square")
+    const matrixBoxes = [];
+    boxes.forEach(box =>{
+        if (box.classList.contains("correctBox")){
+            matrixBoxes.push(boxCorrect);
+        } else 
+        if (box.classList.contains("semiCorrectBox")){
+            matrixBoxes.push(boxSemiCorrect);
+        } else
+        if (box.classList.contains("wrongBox")){
+            matrixBoxes.push(boxWrong);
+    }});
+    const numeroTentativas = matrixBoxes.length/num;
+    var fullmsg = `${title}: mcaurelio.com (${numeroTentativas}/6) \n`;
+    for (var i = 0; i<matrixBoxes.length; i++){
+        if (i > 0 && i< matrixBoxes.length && i%num==0){
+            fullmsg += "\n";
+        }
+        fullmsg += matrixBoxes[i];
+    }
+    const shareButton = document.querySelectorAll(".shareButton");
+    shareButton[0].addEventListener('click', () => {
+        navigator.clipboard.writeText(fullmsg);
+        if (navigator.share != undefined){
+            navigator.share({
+                text: `${fullmsg}`
+            }).then(() =>{
+                const nots = document.getElementById("game__notification");
+                nots.classList.add("animationNotification");
+                nots.textContent = "Copiado para ctrl+V";
+                window.setTimeout(()=>{nots.classList.remove("animationNotification")}, 2500)
+            })
+            .catch((err) => console.error(err));
+}})}
+    
